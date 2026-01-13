9e72ca1e2a43e3d58027000ee138ddecf88378df
Anuja Wanigasekara <anuja.ugc123@gmail.com>
2026-01-13 09:19:21 +0530

    updated QuestionService.java

--- QuestionService.java
+++ QuestionService.java
@@ -2,6 +2,10 @@ public class QuestionService {
 
     Questions[] question = new Questions[5];
 
+    public QuestionService(){
+        question[0] = new Questions();
+    }
+
     public void displayQuestion(){
         System.out.println("Display questions here");
     }
